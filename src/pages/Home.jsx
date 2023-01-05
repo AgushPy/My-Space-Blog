9d3e23b929da622344d3686d5f0e34635820e257
Agush <agusvald18@hotmail.com>
2023-01-05 09:09:37 -0300

    arrange

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export const Home = () => {
       <p>This is my space, where found information about me</p>
         <p>I believe that develop it's awesome and fun, I think that languagues of develop is very important</p>
         <p>If develop not is for all, it's skill awesome for understand how to comportate websites</p>
-        <p>By this motive is very important that people understand concepts of progamming</p>
+        <p>By this motive is very important that people understand concepts of programming</p>
         <p>Different ways for learn programming however, you should pacience and selfthaug</p>
         <p>Never stop learn this world is beatifull, you can aport of yourself in he</p>
       </div>
